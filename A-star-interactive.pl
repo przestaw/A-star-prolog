26b5c1528556c93e61c4bed66cd5b1a24e2dc0a8
przestaw <przemek.stawczyk@gmail.com>
2020-06-04 12:16:35 +0200

    A-star modified algorithm execise

--- A-star-interactive.pl
+++ A-star-interactive.pl
@@ -1,116 +1,126 @@
-start_A_star(InitState, PathCost, N, MaxStepLimit):-
-	score(InitState, 0, 0, InitCost, InitScore),
-	search_A_star([node(InitState, nil, nil, InitCost , InitScore )], [], PathCost, N, 1, MaxStepLimit).
+start_A_star(InitState, PathCost, N, StepLimit):-
+    score(InitState, 0, 0, InitCost, InitScore),
+    search_A_star([node(InitState, nil, nil, InitCost , InitScore )], 
+                  [], PathCost, N, 1, StepLimit).
 
-search_A_star(Queue, ClosedSet, PathCost, N, StepCounter, MaxStepLimit):-
-	StepCounter < MaxStepLimit, ! ,
-	write("Krok nr. : "),
-	write(StepCounter), nl,
-	fetch_new(Node, Queue, ClosedSet, RestQueue, N),
-	NewStepCounter is StepCounter + 1,
-	continue(Node, RestQueue, ClosedSet, PathCost, N, NewStepCounter, MaxStepLimit).
+search_A_star(Queue, ClosedSet, PathCost, N, StepCounter, StepLimit):-
+    write("search_A_star - Krok nr. : "), write(StepCounter), nl,
+    fetch_new(Node, Queue, ClosedSet, RestQueue, N),
+    continue(Node, RestQueue, ClosedSet, PathCost, N, StepCounter, StepLimit).
+    
+search_A_star(Queue, ClosedSet, PathCost, N, StepCounter, StepLimit):-
+    write('search_A_star - Osiągnięto limit krokow. Zwiekszyc limit? (t/n)'), nl,
+    read('t'),
+    NewLimit is StepLimit + 1,
+    search_A_star(Queue, ClosedSet, PathCost, N, StepCounter, NewLimit).
+    
+continue(node(State, Action, Parent, Cost, _), _, 
+        ClosedSet, path_cost(Path, Cost), _, _, _):-
+    write('continue - check goal : '), write(State), nl,
+    goal(State),!,
+    write('continue - reached goal'), nl,
+    build_path(node(Parent, _, _, _, _), ClosedSet, [Action/State], Path).
 
-search_A_star(Queue, ClosedSet, PathCost, N, StepCounter, MaxStepLimit):-
-	write("Krok nr. : "),
-	write(StepCounter), nl,
-	output_nodes(Queue, N, ClosedSet, _),
-	write('Osiągnięto limit krokow. Zwiekszyc limit? (t/n)'), nl,
-	read('t'),
-	NewLimit is MaxStepLimit + 1,
-	fetch_new(Node, Queue, ClosedSet, RestQueue, N),
-	NewStepCounter is StepCounter + 1,
-	continue(Node, RestQueue, ClosedSet, PathCost, N, NewStepCounter, NewLimit).
-
-continue(node(State, Action, Parent, Cost, _), _, ClosedSet, path_cost(Path, Cost), _, _, _):-
-	goal(State),!,
-	build_path(node(Parent, _, _, _, _), ClosedSet, [Action/State], Path).
-
-continue(Node, RestQueue, ClosedSet, Path, N, StepCounter, MaxStepLimit):-
-	expand(Node, NewNodes),
-	insert_new_nodes(NewNodes, RestQueue, NewQueue),
-	search_A_star(NewQueue, [Node| ClosedSet], Path, N, StepCounter, MaxStepLimit).
+continue(Node, RestQueue, ClosedSet, Path, N, StepCounter, StepLimit):-
+    StepCounter < StepLimit,
+    write('continue '), write(StepCounter), 
+    write(' expand : '), write(Node), nl,
+    expand(Node, NewNodes),
+    insert_new_nodes(NewNodes, RestQueue, NewQueue),
+    NewStepCounter is StepCounter + 1,
+    search_A_star(NewQueue, [Node| ClosedSet], Path, 
+                  N, NewStepCounter, StepLimit).
 
 fetch_new(Node, Queue, ClosedSet, RestQueue, N):-
-	get_user_decisions(Queue, N, ClosedSet, Decisions),
-	get_index(Index, Decisions),
-	get_element_at_index(Index, Node, Queue, ClosedSet, RestQueue).
+    get_user_decisions(Queue, N, ClosedSet, Decisions),
+    get_index(Index, Decisions),
+    get_element_at_index(Index, Node, Queue, ClosedSet, RestQueue).
 
 get_user_decisions(Queue, N, ClosedSet, Decisions):-
-	output_nodes(Queue, N, ClosedSet, Diff),
-	write('Podaj indeksy: '), nl,
-	NewN is N - Diff,
-	input_decisions(NewN, Decisions).
+    output_nodes(Queue, N, ClosedSet, Diff, 1),
+    write('Podaj indeksy wezlow: '), nl,
+    VariantsNo is N - Diff,
+    input_decisions(VariantsNo, Decisions).
 
-output_nodes(_, 0, _, 0):- ! .
+output_nodes(_, 0, _, 0, _):- ! .
 
-output_nodes([], N, _, N).
+output_nodes([], N, _, N, _).
 
-output_nodes([X|R], N, ClosedSet, N2):-
-	member(X, ClosedSet), !,
-	output_nodes(R, N, ClosedSet, N2).
+output_nodes([node(State, _, _, _, _)|R], N, ClosedSet, N2, F):-
+    member(node(State, _, _, _, _), ClosedSet), !,
+    output_nodes(R, N, ClosedSet, N2, F).
 
-output_nodes([X|R], N, ClosedSet, N2):-
-	write(X), nl,
-	NewN is N - 1,
-	output_nodes(R, NewN, ClosedSet, N2).
+output_nodes([X|R], N, ClosedSet, N2, F):-
+    write(F), write(' - '), write(X), nl,
+    NewN is N - 1,
+    NewF is F + 1,
+    output_nodes(R, NewN, ClosedSet, N2, NewF).
 
 input_decisions(0, []):- ! .
 
 input_decisions(N, [D|RestDecisions]):-
-	read(D),
-	NewN is N - 1,
-	input_decisions(NewN, RestDecisions).
+    read(D),
+    NewN is N - 1,
+    input_decisions(NewN, RestDecisions).
 
 get_index(X, [X|_]).
 
 get_index(X, [_|R]):-
-	get_index(X, R).
+    get_index(X, R).
 
-get_element_at_index(1, X, [X|R], ClosedSet, R):-
-	\+ member(X, ClosedSet), ! .
+get_element_at_index(1, node(State, Action, Parent, Cost, Score),
+                     [node(State, Action, Parent, Cost, Score)|R],
+                     ClosedSet, R):-
+    \+ member(node(State, _, _, _, _), ClosedSet), ! .
 
-get_element_at_index(Index, Node, [X|R], ClosedSet,[X|RestQueue]):-
-	\+ member(X, ClosedSet), ! ,
-	NewIndex is Index - 1,
-	get_element_at_index(NewIndex, Node, R, ClosedSet, RestQueue).
+get_element_at_index(Index, Node, 
+                     [node(State, Action, Parent, Cost, Score)|R],
+                     ClosedSet,
+                     [node(State, Action, Parent, Cost, Score)|RestQueue]):-
+    \+ member(node(State, _, _, _, _), ClosedSet), ! ,
+    NewIndex is Index - 1,
+    get_element_at_index(NewIndex, Node, R, ClosedSet, RestQueue).
 
 get_element_at_index(Index, Node, [X|R], ClosedSet, [X|RestQueue]):-
-	get_element_at_index(Index, Node, R, ClosedSet, RestQueue).
+    get_element_at_index(Index, Node, R, ClosedSet, RestQueue).
 
 expand(node(State, _ ,_ , Cost, _ ), NewNodes):-
-	findall(node(ChildState, Action, State, NewCost, ChildScore),
-			(succ(State, Action, StepCost, ChildState), score(ChildState, Cost, StepCost, NewCost, ChildScore)),
-			NewNodes), ! .
+    findall(node(ChildState, Action, State, NewCost, ChildScore),
+            (succ(State, Action, StepCost, ChildState),
+                  score(ChildState, Cost, StepCost, NewCost, ChildScore)),
+            NewNodes), ! .
 
 score(State, ParentCost, StepCost, Cost, FScore):-
-	Cost is ParentCost + StepCost,
-	hScore(State, HScore),
-	FScore is Cost + HScore.
+    Cost is ParentCost + StepCost,
+    hScore(State, HScore),
+    FScore is Cost + HScore.
 
 insert_new_nodes( [ ], Queue, Queue) .
 
 insert_new_nodes( [Node|RestNodes], Queue, NewQueue):-
-	insert_p_queue(Node, Queue, Queue1),
-	insert_new_nodes( RestNodes, Queue1, NewQueue) .
+    insert_p_queue(Node, Queue, Queue1),
+    insert_new_nodes( RestNodes, Queue1, NewQueue) .
 
 insert_p_queue(Node,  [ ], [Node] ):- ! .
 
 insert_p_queue(node(State, Action, Parent, Cost, FScore),
-		[node(State1, Action1, Parent1, Cost1, FScore1)|RestQueue],
-		[node(State1, Action1, Parent1, Cost1, FScore1)|Rest1]):-
-	FScore >= FScore1, ! ,
-	insert_p_queue(node(State, Action, Parent, Cost, FScore), RestQueue, Rest1).
+        [node(State1, Action1, Parent1, Cost1, FScore1)|RestQueue],
+        [node(State1, Action1, Parent1, Cost1, FScore1)|Rest1]):-
+    FScore >= FScore1, ! ,
+    insert_p_queue(node(State, Action, Parent, Cost, FScore), RestQueue, Rest1).
 
-insert_p_queue(node(State, Action, Parent, Cost, FScore), Queue, [node(State, Action, Parent, Cost, FScore)|Queue]).
+insert_p_queue(node(State, Action, Parent, Cost, FScore), Queue, 
+        [node(State, Action, Parent, Cost, FScore)|Queue]).
 
 build_path(node(nil, _, _, _, _ ), _, Path, Path):- ! .
 
 build_path(node(EndState, _ , _ , _, _ ), Nodes, PartialPath, Path):-
-	del(Nodes, node(EndState, Action, Parent , _ , _  ) , Nodes1) ,
-	build_path(node(Parent,_ ,_ , _ , _ ) , Nodes1, [Action/EndState|PartialPath],Path).
+    del(Nodes, node(EndState, Action, Parent , _ , _  ) , Nodes1) ,
+    build_path(node(Parent,_ ,_ , _ , _ ), Nodes1, 
+              [Action/EndState|PartialPath],Path).
 
 del([X|R],X,R).
 
 del([Y|R],X,[Y|R1]):-
-	X\=Y,
-	del(R,X,R1). 
+    X\=Y,
+    del(R,X,R1). 
--- roadmap.pl
+++ roadmap.pl
@@ -1,23 +1,25 @@
 % example grid
-succ(a,ab,2,b).
-succ(b,bf,3,f).
-succ(a,ac,3,c).
-succ(b,bg,4,g).
-succ(g,gm,2,m).
-succ(c,cd,2,d).
-succ(d,dm,2,m).
-succ(c,ce,3,e).
-succ(e,em,5,m).
+succ(a, a-b, 2, b).
+succ(a, a-c, 3, c).
+succ(b, b-g, 4, g).
+succ(b, b-f, 3, f).
+succ(c, c-d, 2, d).
+succ(c, c-e, 3, e).
+succ(g, g-m, 2, m).
+succ(f, f-h, 4, h).
+succ(d, d-m, 2, m).
+succ(e, e-m, 5, m).
 
 % set goal
 goal(m).
 
 % basic heuristic
-hScore(a,4).
-hScore(b,4).
-hScore(f,7).
-hScore(g,1).
-hScore(m,0).
-hScore(c,3).
-hScore(d,1).
-hScore(e,4).
+hScore(a, 4).
+hScore(b, 4).
+hScore(c, 3).
+hScore(d, 1).
+hScore(e, 4).
+hScore(f, 7).
+hScore(g, 1).
+hScore(h, 3).
+hScore(m, 0).
